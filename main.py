d467595e07fe665d5a5ac5e5315cef85e676d7ef
admin <kpumgagnani@gmail.com>
2024-03-18 05:49:21 +0530

    Freeze Commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from flask_frozen import Freezer
 
 app = Flask(__name__)
+freezer = Freezer(app)
 
 
 @app.route('/')
@@ -10,4 +12,3 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
